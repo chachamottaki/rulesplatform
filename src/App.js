e2359958f34df4f8c7d5e9cd7dc827cc214afa33
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-04-23 15:41:23 +0200

    added connection to RulesEngine API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('https://localhost:7113')
+    .then((response) => {
+      console.log(response.data)
+    })
+  }, [])
   return (
     <div className="App">
       <h1>
