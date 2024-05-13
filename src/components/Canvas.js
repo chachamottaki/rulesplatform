4ede09ca424b0bb5dab456907c9447711b702dec
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-13 10:16:07 +0200

    made canvas nodes draggable

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,6 +1,29 @@
 import React, { useState } from 'react';
-import { useDrop } from 'react-dnd';
+import { useDrop, useDrag } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
+
+// Draggable component for nodes on the canvas
+const DraggableCanvasNode = ({ id, name, type, left, top }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.NODE,
+    item: { id, name, type, left, top },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div ref={drag} style={{
+      position: 'absolute',
+      left,
+      top,
+      opacity: isDragging ? 0.5 : 1,
+      cursor: 'move'
+    }}>
+      {name}
+    </div>
+  );
+};
 
 const Canvas = () => {
   const [nodes, setNodes] = useState([]);
@@ -11,16 +34,25 @@ const Canvas = () => {
       const delta = monitor.getDifferenceFromInitialOffset();
       const left = Math.round((item.left || 0) + delta.x);
       const top = Math.round((item.top || 0) + delta.y);
-      setNodes(oldNodes => [...oldNodes, { ...item, left, top }]);
+
+      // Update existing nodes or add new ones
+      setNodes(oldNodes => {
+        const existingNode = oldNodes.find(node => node.id === item.id);
+        if (existingNode) {
+          return oldNodes.map(node =>
+            node.id === item.id ? { ...node, left, top } : node
+          );
+        } else {
+          return [...oldNodes, { ...item, left, top }];
+        }
+      });
     },
   });
 
   return (
     <div ref={drop} style={{ width: '100%', height: '500px', position: 'relative', border: '1px solid black' }}>
       {nodes.map((node, index) => (
-        <div key={index} style={{ position: 'absolute', left: node.left, top: node.top }}>
-          {node.name}  
-        </div>
+        <DraggableCanvasNode key={index} {...node} />
       ))}
     </div>
   );
