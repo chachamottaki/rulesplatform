c196d0e4bc8876762172352301b8f0af1fa6b8af
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-13 09:19:49 +0200

    when node placed, name of the placed node is shown

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -13,13 +13,13 @@ const Canvas = () => {
       const top = Math.round((item.top || 0) + delta.y);
       setNodes(oldNodes => [...oldNodes, { ...item, left, top }]);
     },
-  });  
+  });
 
   return (
-    <div ref={drop} style={{ width: '100%', height: '500px', position: 'relative' }}>
+    <div ref={drop} style={{ width: '100%', height: '500px', position: 'relative', border: '1px solid black' }}>
       {nodes.map((node, index) => (
         <div key={index} style={{ position: 'absolute', left: node.left, top: node.top }}>
-          Node
+          {node.name}  
         </div>
       ))}
     </div>
--- src/components/DraggableNode.js
+++ src/components/DraggableNode.js
@@ -3,20 +3,19 @@ import { useDrag } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 
 const DraggableNode = ({ id, name, type }) => {
-    const [{ isDragging }, drag] = useDrag({
-      type: ItemTypes.NODE,
-      item: { id, type },
-      collect: monitor => ({
-        isDragging: !!monitor.isDragging(),
-      }),
-    });
-  
-    return (
-      <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1, cursor: 'grab' }}>
-        {name}
-      </div>
-    );
-  };
-  
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.NODE,
+    item: { id, name, type },  // Include 'name' in the drag item data
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1, cursor: 'grab' }}>
+      {name}
+    </div>
+  );
+};
 
 export default DraggableNode;
