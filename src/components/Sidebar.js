aaadd69679878dfa43605a5f2c4b9c29f160e8da
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2024-05-23 16:03:18 +0200

    updated sidebar layout to match TPST a bit better

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Sidebar = () => {
   return (
@@ -7,10 +8,14 @@ const Sidebar = () => {
       <h3>Navigation</h3>
       <ul>
         <li>
-          <Link to="/">Device Config</Link>
+          <Link to="/"> 
+          <i className="fas fa-upload icon"></i>Device Config
+          </Link>
         </li>
         <li>
-          <Link to="/rule-chains">Rule Chains</Link>
+          <Link to="/rule-chains">
+          <i className="fas fa-sitemap icon"></i> Rule Chains
+          </Link>
         </li>
       </ul>
     </div>
